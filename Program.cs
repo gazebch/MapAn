37203e8ede273fd146336f0cee4478f703f0d3c0
Gazeba <sipion46@mail.ru>
2022-06-05 16:37:50 +0300

    Удалил строчку

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asdsad");
             var rand = new Random();
             List<Anim> CatalogAnimals = new List<Anim>();
             var x = int.Parse(Test());
